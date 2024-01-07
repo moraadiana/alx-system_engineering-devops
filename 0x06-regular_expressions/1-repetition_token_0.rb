632904ecd0c43ae1df16f57d60cb30c616e4f889
moraadiana <97588455+moraadiana@users.noreply.github.com>
2024-01-07 14:28:54 +0300

    d

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
-# File: 1-repetition_token_0.rb
-
-def match_repetition_token(input)
-    # Your regular expression goes here
-    regex = /your_regular_expression/
-  
-    # Check if the input matches the regular expression
-    if input.match?(regex)
-      puts "Input matches the repetition token!"
-    else
-      puts "Input does not match the repetition token."
-    end
-  end
-  
-  # Accept one argument from the command line
-  input_argument = ARGV[0]
-  
-  # Call the method with the input argument
-  match_repetition_token(input_argument)
-  
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt{2,5}n/).join
